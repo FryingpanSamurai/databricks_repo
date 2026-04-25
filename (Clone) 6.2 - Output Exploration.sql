8e72d02275c30ba8160e5b3c6ebcd9d7b11b7de7
FryingpanSamurai <bpatricka@live.com>
2026-04-25 04:46:06 +0000

    up to chapter 6
    Lakeflow and CDC with CDF table

---
+++ (Clone) 6.2 - Output Exploration.sql
@@ -0,0 +1,11 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC # Examining DLT pipelines
+
+-- COMMAND ----------
+
+DESCRIBE HISTORY de_associate_school.enrollments_silver
+
+-- COMMAND ----------
+
+DESCRIBE HISTORY de_associate_school.courses_silver
